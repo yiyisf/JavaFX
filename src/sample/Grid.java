76b1d29c8e82d5f0901f77052561e44d479f94ec
yiyi <yiyi119120@gmail.com>
2016-09-15 18:28:52 +0800

    add gridpane

---
+++ src/sample/Grid.java
@@ -0,0 +1,71 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+/**
+ * Created by liuzhe on 16/9/15.
+ */
+public class Grid extends Application{
+
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("yiyi");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(8);
+
+        //name label
+        Label name = new Label("username:");
+        GridPane.setConstraints(name, 0, 0);
+
+        //name input
+        TextField inname = new TextField("pls input your name");
+        GridPane.setConstraints(inname, 1, 0);
+
+        //password label
+        Label pass = new Label("password:");
+        GridPane.setConstraints(name, 0, 1);
+
+        //password input
+        PasswordField inpass = new PasswordField();
+        GridPane.setConstraints(inname, 1, 1);
+
+        gridPane.addRow(0, name, inname);
+        gridPane.addRow(1, pass, inpass);
+
+        Scene scene = new Scene(gridPane, 400, 300);
+
+        window.setScene(scene);
+
+        window.show();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
